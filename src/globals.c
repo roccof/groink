b9810da89f79b8fb3a0d7a7be69c7ba9a717cb17
LordZen <lordzen@autistici.org>
2011-03-17 13:00:38 +0100

    several fixes

---
+++ src/globals.c
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) Denatured Ethyl Crew
+ *
+ * This file is part of GroinK.
+ *
+ * GroinK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GroinK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GroinK.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+
+#include "base.h"
+#include "debug.h"
+#include "globals.h"
+/* #include "host.h" */
+#include "list.h"
+/* #include "mitm.h" */
+/* #include "script_engine.h" */
+/* #include "capture.h" */
+
+void globals_init()
+{
+  gbls = (struct globals_t *)safe_alloc(sizeof(struct globals_t));
+
+  /*** Set default values ***/
+
+  gbls->iface = NULL;
+  gbls->promisc = 1;
+  gbls->rfmon = 0;
+  gbls->daemon = 0;
+  gbls->scan = 1;
+
+  list_init(&(gbls->hosts));
+
+  gbls->mtu = 0;
+  gbls->link_addr = NULL;
+  gbls->net_addr = NULL;
+  gbls->net6_addr = NULL;
+  gbls->netmask = NULL;
+
+  gbls->sockfd = -1;
+  gbls->sockfd6 = -1;
+
+  /* gbls->pcap = NULL; */
+  /* gbls->dlt = 0; */
+  /* gbls->cap_packets = 0; */
+  /* gbls->cs = CAP_STATE_NONE; */
+  /* gbls->snaplen = CAPTURE_SNAPLEN; */
+  /* gbls->decode = 1; */
+  /* gbls->cap_timeout = CAPTURE_TIMEOUT; */
+
+  /* gbls->L = NULL; */
+  /* gbls->script = NULL; */
+  /* bzero(gbls->script_argv, MAX_SCRIPT_ARGS); */
+  /* gbls->script_argc = 0; */
+  /* gbls->script_debug_mode = 0; */
+  /* gbls->scripts_dir = NULL; */
+
+  gbls->mitm = NULL;
+  /* gbls->mitm_state = MITM_STATE_STOP; */
+  gbls->mitm_options = NULL;
+}
+
+void globals_destroy()
+{
+  if(gbls == NULL)
+    return;
+
+  list_free(&(gbls->hosts), &remove_host_cb);
+  
+  if(gbls->link_addr != NULL)
+    free(gbls->link_addr);
+  
+  if(gbls->net_addr != NULL)
+    free(gbls->net_addr);
+  
+  if(gbls->netmask != NULL)
+    free(gbls->netmask);
+  
+  if(gbls->mitm != NULL)
+    free(gbls->mitm);
+  
+  if(gbls->script != NULL)
+    free(gbls->script);
+  
+  free(gbls);
+  gbls = NULL;
+}
---
+++ src/globals.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) Denatured Ethyl Crew
+ *
+ * This file is part of GroinK.
+ *
+ * GroinK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GroinK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GroinK.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef GROINK_GLOBALS_H
+#define GROINK_GLOBALS_H
+
+#include <lua.h>
+#include <pcap.h>
+
+/* #include "capture.h" */
+/* #include "list.h" */
+/* #include "mitm.h" */
+/* #include "script_engine.h" */
+
+struct globals_t
+{
+  /* General */
+  char *iface;                            /* Network interface */
+  int promisc;                            /* Promisc mode */
+  int rfmon;                              /* Monitor mode */
+  int daemon;                             /* Is daemon mode? */
+  int scan;                               /* Host scanning */
+
+  List hosts;                             /* List of all hosts present in the same network */
+
+  /* Iface info */
+  int mtu;                                /* Maximum transfer unit */
+  char *link_addr;                        /* Hardware address */
+  char *net_addr;                         /* Network ipv4 address */
+  char *net6_addr;                        /* Network ipv6 address */
+  char *netmask;                          /* Network mask */
+
+  /* Packet forwarding */
+  int sockfd;
+  int sockfd6;
+  
+  /* Capture engine */
+  pcap_t *pcap;                           /* Pcap handler */
+  int dlt;                                /* Data link type  */
+  long cap_packets;                       /* Captured packet counter */
+  int cs;                                 /* State of capture engine */
+  int snaplen;                            /* Captured packet length */
+  int decode;                             /* If 1 the packet is decoded */
+  int cap_timeout;                        /* Pcap capture packet timeout*/
+
+  /* Script engine */
+  lua_State *L;                           /* Lua state */
+  char *script;                           /* Script to run */
+  /* char *script_argv[MAX_SCRIPT_ARGS]; */     /* Arguments of script */
+  int script_argc;                        /* Number of argument of script */
+  int script_debug_mode;                  /* Script debug mode */
+  char *scripts_dir;                      /* Scripts directory */
+
+  /* MiTM attacks */
+  char *mitm;                             /* MiTM attack name */
+  /* MiTM_State mitm_state; */                  /* MiTM state */
+  char *mitm_options;
+};
+
+struct globals_t *gbls;
+
+void globals_init();
+void globals_destroy();
+
+#endif /* GROINK_GLOBALS_H */
