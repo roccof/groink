48fa93e974dd172108955c44be0b7fd60eee129c
LordZen <lordzen@autistici.org>
2011-05-11 02:05:38 +0200

    testing enviroment

---
+++ tests/netutil_test.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) Denatured Ethyl Crew
+ *
+ * This file is part of GroinK.
+ *
+ * GroinK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GroinK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GroinK.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <base.h>
+#include <netutil.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  /* _uint8 *addr = NULL; */
+  /* char *saddr = NULL; */
+
+  /* assert(is_ipv6_addr("::1")); */
+  /* assert(!is_ipv6_addr("192.168.0.1")); */
+
+  /* addr = ipv6_addr_aton("::1"); */
+  /* assert(addr != NULL); */
+
+  /* saddr = ipv6_addr_ntoa(addr); */
+  /* assert(saddr != NULL); */
+  /* printf(">>> ADDR: %s", saddr); */
+
+  /* free(addr); */
+  /* free(saddr); */
+
+  return 0;
+}
