21e754c4e56f9faf7c24a8a2e3402410e74b741b
Luca Bruno <lucabru@src.gnome.org>
2011-09-15 10:54:20 +0200

    buildsys: Add autogen.sh for easily building from git

---
+++ autogen.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+cd $srcdir
+
+mkdir -p m4
+autoreconf -v --install || exit 1
+cd $ORIGDIR || exit $?
+
+if test -z "$NOCONFIGURE"; then
+    $srcdir/configure "$@"
+fi
